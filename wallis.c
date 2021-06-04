ba16360ea15df387f36c0d14e8096f0dbf2be401
ACKSA17 <85336992+ACKSA17@users.noreply.github.com>
2021-06-04 07:58:22 +0000

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -16,18 +16,6 @@ for(i=1;i<=n;i++)
  }
  return 2*pi;
 }
-float mc_pi(int n)
-{
-int circle=0;
-float x,y;
-for(int i=1;i<=n;i++)
-  {
-    x=frandom();
-    y=frandom();
-    if(x*x + y*y <= 1){circle++;}
-  }
-  return 4*((float)circle/n);
-}
 
   
   
